d7d8414962cbb8941a0e538f89b1ff1b0b16b971
davyuu <davidyu_2005@hotmail.com>
2017-03-11 21:21:01 -0500

    Add sample list component

---
+++ app/components/ListViewComponent.js
@@ -0,0 +1,35 @@
+import React, {Component} from 'react';
+import {ListView, Text, View} from 'react-native';
+
+export default class ListViewComponent extends Component {
+    constructor(props) {
+        super(props);
+        const ds = new ListView.DataSource({
+            rowHasChanged: (r1, r2) => r1 !== r2
+        });
+        this.state = {
+            dataSource: ds.cloneWithRows([
+                'John',
+                'Joel',
+                'James',
+                'Jommy',
+                'Jackson',
+                'Jillian',
+                'Julie'
+            ])
+        };
+    }
+    render() {
+        return (
+            <View style={{
+                flex: 1,
+                paddingTop: 22
+            }}>
+                <ListView
+					dataSource={this.state.dataSource}
+					renderRow={(rowData) => <Text>{rowData}</Text>}
+				/>
+			</View>
+		);
+	}
+}
