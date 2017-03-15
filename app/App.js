62d5cf3681e68eb473dc5a2f826cf308bbd32891
David Yu <davidyu_2005@hotmail.com>
2017-03-14 22:45:15 -0400

    add splash page

---
+++ app/App.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+import {View} from 'react-native';
+import Splash from "./components/Splash"
+
+export default class App extends Component {
+    render() {
+        return (
+            <Splash />
+        )
+    }
+}
--- index.android.js
+++ index.android.js
@@ -1,20 +1,4 @@
-import React, {Component} from 'react';
-import {AppRegistry, Text, View} from 'react-native';
-import Component1 from "./app/components/Component1"
+import {AppRegistry} from 'react-native';
+import App from "./app/App"
 
-class ReactApp1 extends Component {
-    render() {
-        return (
-            <View style={{
-                alignItems: 'center',
-                margin: 80
-            }}>
-                <Component1 name="name1"/>
-                <Component1 name="name3"/>
-                <Component1 name="name5"/>
-            </View>
-        )
-    }
-}
-
-AppRegistry.registerComponent('ReactApp1', () => ReactApp1);
+AppRegistry.registerComponent('ReactApp1', () => App);
