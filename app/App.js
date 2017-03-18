990e1d746ae9b5caae2b9826a89ece56c166b8e4
David Yu <davidyu_2005@hotmail.com>
2017-03-18 16:12:48 -0400

    create login page.

--- app/App.js
+++ app/App.js
@@ -1,11 +1,12 @@
 import React, {Component} from 'react';
 import {View} from 'react-native';
-import Splash from "./components/Splash"
+import Splash from "./components/Splash/Splash"
+import Login from "./components/Login/Login"
 
 export default class App extends Component {
     render() {
         return (
-            <Splash />
+            <Login />
         )
     }
 }
--- app/components/Splash/Splash.js
+++ app/components/Splash/Splash.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {StyleSheet, View, Text} from 'react-native';
 
 export default class Splash extends Component {
     render() {
